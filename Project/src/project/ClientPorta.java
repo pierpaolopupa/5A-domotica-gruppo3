707802c3a43d5d530a5aabacb1dfc5f04a947fd5
Nogaye <pierpaolo.pupa@gmail.com>
2025-11-13 09:44:58 +0100

    prima parte -facchinetti

--- Project/src/project/ClientPorta.java
+++ Project/src/project/ClientPorta.java
@@ -1,5 +1,18 @@
 package project;
 
-public class ClientPorta {
-    
-}
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class ClientPorta
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            // collega il sensore al server
+            Socket s = new Socket("127.0.0.1", 12345); // IP e porta del server
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+            Scanner tastiera = new Scanner(System.in);
+            }}
